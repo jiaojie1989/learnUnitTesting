533c5f8941b5b1eb67437c1087b59b0218ae5b81
JIAO Jie <thomasjiao@vip.qq.com>
2017-02-10 17:18:29 +0800

    init @ Fri Feb 10 17:18:29 CST 2017

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * Copyright (C) 2017 SINA Corporation
+ *  
+ *  
+ * 
+ * This script is firstly created at 2017-02-09.
+ * 
+ * To see more infomation,
+ *    visit our official website http://app.finance.sina.com.cn/.
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
